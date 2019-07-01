83e5021c750c58dd7b5a1f8dd55227a5ddc38ac7
ZipCPU <dgisselq@ieee.org>
2019-07-01 12:31:41 -0400

    Updated rounding (convround) to handle too many output bits

--- rtl/convround.v
+++ rtl/convround.v
@@ -17,7 +17,7 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 //
-// Copyright (C) 2015-2018, Gisselquist Technology, LLC
+// Copyright (C) 2015-2019, Gisselquist Technology, LLC
 //
 // This file is part of the general purpose pipelined FFT project.
 //
@@ -68,13 +68,20 @@ module	convround(i_clk, i_ce, i_val, o_val);
 	// effect here.
 
 		always @(posedge i_clk)
-			if (i_ce)	o_val <= i_val[(IWID-1):0];
+		if (i_ce)	o_val <= i_val[(IWID-1):0];
+
+	end else if (IWID-SHIFT < OWID)
+	begin // No truncation or rounding, output drops no bits
+	// Instead, we need to stuff the bits in the output
+
+		always @(posedge i_clk)
+		if (i_ce)	o_val <= { {(OWID-IWID+SHIFT){i_val[IWID-SHIFT-1]}}, i_val[(IWID-SHIFT-1):0] };
 
 	end else if (IWID-SHIFT == OWID)
 	begin // No truncation or rounding, output drops no bits
 
 		always @(posedge i_clk)
-			if (i_ce)	o_val <= i_val[(IWID-SHIFT-1):0];
+		if (i_ce)	o_val <= i_val[(IWID-SHIFT-1):0];
 
 	end else if (IWID-SHIFT-1 == OWID)
 	begin // Output drops one bit, can only add one or ... not.
@@ -86,20 +93,21 @@ module	convround(i_clk, i_ce, i_val, o_val);
 		assign	first_lost_bit = i_val[0];
 
 		always @(posedge i_clk)
-			if (i_ce)
-			begin
-				if (!first_lost_bit) // Round down / truncate
-					o_val <= truncated_value;
-				else if (last_valid_bit)// Round up to nearest
-					o_val <= rounded_up; // even value
-				else // else round down to the nearest
-					o_val <= truncated_value; // even value
-			end
+		if (i_ce)
+		begin
+			if (!first_lost_bit) // Round down / truncate
+				o_val <= truncated_value;
+			else if (last_valid_bit)// Round up to nearest
+				o_val <= rounded_up; // even value
+			else // else round down to the nearest
+				o_val <= truncated_value; // even value
+		end
 
 	end else // If there's more than one bit we are dropping
 	begin
 		wire	[(OWID-1):0]	truncated_value, rounded_up;
 		wire			last_valid_bit, first_lost_bit;
+
 		assign	truncated_value=i_val[(IWID-1-SHIFT):(IWID-SHIFT-OWID)];
 		assign	rounded_up=truncated_value + {{(OWID-1){1'b0}}, 1'b1 };
 		assign	last_valid_bit = truncated_value[0];
